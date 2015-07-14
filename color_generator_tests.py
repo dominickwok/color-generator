9721f35c3157520e4b419bf5f86c2e42313e6b7d
dominickwok <dominictskwok@gmail.com>
2015-07-14 02:37:18 -0400

    tests for run_color_generator function

--- color_generator_tests.py
+++ color_generator_tests.py
@@ -35,37 +35,26 @@ assert rgb_to_hex((131, 110, 177)) == "#836eb1"
 assert rgb_to_hex((0, 0, 0)) == "#000000"
 assert rgb_to_hex((255, 255, 255)) == "#ffffff"
 
-# hex_shades test
-output_shades("#eeeeee", 5)
-output_shades("e94297", 5)
-"""
+# output_shades test
 assert isinstance(output_shades("#eeeeee", 5), list), "hex = #eeeeee and num_shades = 5 did not produce a list"
 assert isinstance(output_shades("#eee", 19), list), "hex = #eee and num_shades = 19 did not produce a list"
 assert isinstance(output_shades("e94297", 5), list), "hex = #e94297 and num_shades = 5 did not produce a list"
-assert hex_shades("e94297", 'abc')[0:5] == "error", "non-numeric value for num_shades accepted"
-assert hex_shades("e94297", 5.3)[0:5] == "error", "float num_shades value accepted"
-assert hex_shades("e94297", -5)[0:5] == "error", "negative num_shades value accepted"
-assert hex_shades("zzzzzz", 5)[0:5] == "error", "non-hex value accepted"
-assert hex_shades("#", -5)[0:5] == "error", "non-hex value accepted"
-assert hex_shades(123456, 4)[0:5] == "error", "non-string value accepted"
 
-# in theory, this test is now really part of shade bounds testing
+# run_color_generator test
+assert isinstance(run_color_generator("#eeeeee", 5), list), "hex = #eeeeee and num_shades = 5 did not produce a list"
+assert isinstance(run_color_generator("#eee", 19), list), "hex = #eee and num_shades = 19 did not produce a list"
+assert isinstance(run_color_generator("e94297", 5), list), "hex = #e94297 and num_shades = 5 did not produce a list"
+
+assert run_color_generator("e94297", 'abc')[0:5] == "error", "non-numeric value for num_shades accepted"
+assert run_color_generator("e94297", 5.3)[0:5] == "error", "float num_shades value accepted"
+assert run_color_generator("e94297", -5)[0:5] == "error", "negative num_shades value accepted"
+assert run_color_generator("zzzzzz", 5)[0:5] == "error", "non-hex value accepted"
+assert run_color_generator("#", -5)[0:5] == "error", "non-hex value accepted"
+assert run_color_generator(123456, 4)[0:5] == "error", "non-string value accepted"
+
 for i in range(1, 20):
 	#print "testing num_shades = %s..." % i
-	num = len(hex_shades("e94297", i))
+	num = len(run_color_generator("e94297", i))
 	assert num-i==0, "ER: num_shades=%s, AR: num_shades=%s" % (i, num)
 
 print "all tests passed!"
-"""
-
-
-print "all tests passed!!"
-
-"""
-# shade_bounds tests
-for i in range (1, 20):
-	result = shade_bounds(i)
-	assert result[0] > 0, "ER: start_index is greater than zero, AR: start_index = %s" % (result[0])
-	assert result[1] < 21, "ER: end_index less than max, AR: end_index = %s" % (result[1])
-	assert result[2] == 1 or result[2] == 2, "ER: step is 1 or 2; AR: step = %s" % (result[2])
-"""
